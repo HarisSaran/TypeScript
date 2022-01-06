b4e9a48a9b790b53ba0f1a8ee20f1b2f14023b2d
Haris Saran <harissaran33@gmail.com>
2022-01-05 19:31:41 -0600

    practice with number, string, boolean, object, array, typle

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Code goes here!!!");
+// We can do it like this but better the other way to let type script infer it for us
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Haris",
+//   age: 30,
+// };
+var person = {
+    name: "Haris",
+    age: 30,
+    hobbies: ["Sports", "camping"],
+    role: [23, "author"]
+};
+person.role.push("admin");
+// person.role[1] = 44;
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map());  // ERROR ts recognizes that map() does not exist with type string.
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,49 @@
-console.log("Code goes here!!!");
+// We can do it like this but better the other way to let type script infer it for us
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Haris",
+//   age: 30,
+// };
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //this allows us to create a tuple, (meaning; must look like this!)
+//                           // Telling ts that we want an array with exatly 2 elements, first element should be a number second should be string
+// } = {
+//   name: "Haris",
+//   age: 30,
+//   hobbies: ["Sports", "camping"],
+//   role: [23, "author"],
+// };
+
+// person.role.push("admin");
+// person.role[1] = 44;
+// person.role = [0, "admin", "user"];
+
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //this allows us to create a tuple, (meaning; must look like this!)
+                          // Telling ts that we want an array with exatly 2 elements, first element should be a number second should be string
+} = {
+  name: "Haris",
+  age: 30,
+  hobbies: ["Sports", "camping"],
+  role: [23, "author"],
+};
+
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map());  // ERROR ts recognizes that map() does not exist with type string.
+}
